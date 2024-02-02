989c01994e4667578aec8c65136f9d3e24f27c6d
davelee2804 <davelee2804@gmail.com>
2024-02-02 14:31:57 +1100

    disabling the clean stage for now

--- lfric_atm-fab-clean.sh
+++ lfric_atm-fab-clean.sh
@@ -2,7 +2,7 @@
 
 cd /scratch/hc46/hc46_gitlab/builds/$CI_RUNNER_SHORT_TOKEN
 echo $PWD
-rm -rf 0/bom/ngm/lfric/lfric_atm-fab
-rm -rf 1/bom/ngm/lfric/lfric_atm-fab
-rm -rf 2/bom/ngm/lfric/lfric_atm-fab
-rm -rf 3/bom/ngm/lfric/lfric_atm-fab
+#rm -rf 0/bom/ngm/lfric/lfric_atm-fab
+#rm -rf 1/bom/ngm/lfric/lfric_atm-fab
+#rm -rf 2/bom/ngm/lfric/lfric_atm-fab
+#rm -rf 3/bom/ngm/lfric/lfric_atm-fab
