134285b1ddee38a08697f8bec00b9e89324b00c8
davelee2804 <davelee2804@gmail.com>
2024-02-05 10:39:16 +1100

    passing the lfric revision to file to be read by the grab lfric task

--- lfric_atm-fab-build.sh
+++ lfric_atm-fab-build.sh
@@ -2,8 +2,7 @@
 
 # get the current lfric revision from the repo mirror
 rev=$(svn info file:///g/data/ki32/mosrs/lfric/LFRic/trunk | grep Revision | sed 's/.* //g')
-echo "revision:"
-echo $rev
+echo $rev > /scratch/hc46/hc46_gitlab/builds/$CI_RUNNER_SHORT_TOKEN/0/bom/ngm/lfric/lfric_atm-fab/lfric_revision
 
 # load the container
 module use /scratch/hc46/hc46_gitlab/ngm/modules/
--- scripts/grab_lfric.py
+++ scripts/grab_lfric.py
@@ -4,13 +4,22 @@
 #  For further details please refer to the file COPYRIGHT
 #  which you should have received as part of this distribution
 # ##############################################################################
+import os
 import sys
 
 from fab.build_config import BuildConfig
 from fab.steps.grab.fcm import fcm_export
 
-LFRIC_REVISION = int(sys.argv[1])
-ROSE_PICKER_REVISION = int(sys.argv[1])
+#LFRIC_REVISION = int(sys.argv[1])
+#ROSE_PICKER_REVISION = int(sys.argv[1])
+
+TOKEN = os.getenv('CI_RUNNER_SHORT_TOKEN')
+rev_file = '/scratch/hc46/hc46_gitlab/builds/'+TOKEN+'/0/bom/ngm/lfric/lfric_atm-fab/lfric_revision'
+
+file=open(rev_file,'r')
+LFRIC_REVISION=file.readline().strip('\n')
+file.close()
+print('lfric revision: ' + LFRIC_REVISION)
 
 # these configs are interrogated by the build scripts
 # todo: doesn't need two separate configs, they use the same project workspace
@@ -25,4 +34,5 @@ if __name__ == '__main__':
 
     with gpl_utils_source_config:
         fcm_export(
-            gpl_utils_source_config, src='fcm:lfric_gpl_utils.xm-tr', revision=ROSE_PICKER_REVISION, dst_label='gpl_utils')
+            gpl_utils_source_config, src='fcm:lfric_gpl_utils.xm-tr', revision=LFRIC_REVISION, dst_label='gpl_utils')
+            #gpl_utils_source_config, src='fcm:lfric_gpl_utils.xm-tr', revision=ROSE_PICKER_REVISION, dst_label='gpl_utils')
