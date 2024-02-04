af1099b320930037eeb675b474c98f1c550ef2f5
davelee2804 <davelee2804@gmail.com>
2024-02-05 10:59:31 +1100

    using separate env variable for the rose picker revision from the lfric revision

--- scripts/grab_lfric.py
+++ scripts/grab_lfric.py
@@ -21,6 +21,8 @@ LFRIC_REVISION=file.readline().strip('\n')
 file.close()
 print('lfric revision: ' + LFRIC_REVISION)
 
+ROSE_PICKER_REVISION=LFRIC_REVISION
+
 # these configs are interrogated by the build scripts
 # todo: doesn't need two separate configs, they use the same project workspace
 lfric_source_config = BuildConfig(project_label=f'lfric source {LFRIC_REVISION}')
@@ -34,5 +36,4 @@ if __name__ == '__main__':
 
     with gpl_utils_source_config:
         fcm_export(
-            gpl_utils_source_config, src='fcm:lfric_gpl_utils.xm-tr', revision=LFRIC_REVISION, dst_label='gpl_utils')
-            #gpl_utils_source_config, src='fcm:lfric_gpl_utils.xm-tr', revision=ROSE_PICKER_REVISION, dst_label='gpl_utils')
+            gpl_utils_source_config, src='fcm:lfric_gpl_utils.xm-tr', revision=ROSE_PICKER_REVISION, dst_label='gpl_utils')
