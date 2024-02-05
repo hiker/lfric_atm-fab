eb5b84485c36bbdbe2ca934ceeb470a4bed21921
davelee2804 <davelee2804@gmail.com>
2024-02-05 19:47:23 +1100

    minor fixes

--- lfric_atm-fab-build.sh
+++ lfric_atm-fab-build.sh
@@ -14,6 +14,8 @@ imagerun FAB_WORKSPACE=$PWD FC=ifort ./scripts/grab_lfric.py
 # build lfric_atm
 imagerun FAB_WORKSPACE=$PWD FC=ifort ./scripts/atm.py
 
+rev='46556'
+
 cp lfric_source_${rev}/source/lfric/lfric_atm/example/configuration.nml /scratch/hc46/hc46_gitlab/builds/$CI_RUNNER_SHORT_TOKEN/0/bom/ngm/lfric/lfric_atm-fab
 
 cp atm_ifort_1stage/lfric_atm.exe /scratch/hc46/hc46_gitlab/builds/$CI_RUNNER_SHORT_TOKEN/0/bom/ngm/lfric/lfric_atm-fab
