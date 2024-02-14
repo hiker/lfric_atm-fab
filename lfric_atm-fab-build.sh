3ecde9d65ccb52153ec09ae44bf66d74d09363bc
davelee2804 <davelee2804@gmail.com>
2024-02-14 11:57:57 +1100

    building/running at a fixed revision for now

--- lfric_atm-fab-build.sh
+++ lfric_atm-fab-build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # get the current lfric revision from the repo mirror
-rev=$(svn info file:///g/data/ki32/mosrs/lfric/LFRic/trunk | grep Revision | sed 's/.* //g')
+#rev=$(svn info file:///g/data/ki32/mosrs/lfric/LFRic/trunk | grep Revision | sed 's/.* //g')
+rev='48813'
 echo $rev > /scratch/hc46/hc46_gitlab/builds/$CI_RUNNER_SHORT_TOKEN/0/bom/ngm/lfric/lfric_atm-fab/lfric_revision
 
 # load the container
