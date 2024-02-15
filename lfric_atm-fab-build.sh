327bc62d289e956889fb42e6d4f15427c94b0c87
davelee2804 <davelee2804@gmail.com>
2024-02-16 10:23:07 +1100

    new branch to build lfric_atm on the compute node

--- lfric_atm-fab-build.sh
+++ lfric_atm-fab-build.sh
@@ -2,8 +2,7 @@
 
 # get the current lfric revision from the repo mirror
 rev=$(svn info file:///g/data/ki32/mosrs/lfric/LFRic/trunk | grep Revision | sed 's/.* //g')
-echo $rev > /scratch/hc46/hc46_gitlab/builds/$CI_RUNNER_SHORT_TOKEN/0/bom/ngm/lfric/lfric_atm-fab/lfric_revision
-echo $rev > lfric_revision
+echo $rev > /scratch/hc46/hc46_gitlab/builds/0/bom/ngm/lfric/lfric_atm-fab/lfric_revision
 
 # load the container
 module use /scratch/hc46/hc46_gitlab/ngm/modules/
@@ -11,13 +10,13 @@ module load lfric-v0/intel-openmpi-lfric-fab
 
 # grab the lfric sources
 #imagerun FAB_WORKSPACE=$PWD FC=ifort ./scripts/grab_lfric.py
-imagerun FAB_WORKSPACE=/scratch/hc46/hc46_gitlab/builds/$CI_RUNNER_SHORT_TOKEN/0/bom/ngm/lfric/lfric_atm-fab/ FC=ifort ./scripts/grab_lfric.py
+imagerun FAB_WORKSPACE=/scratch/hc46/hc46_gitlab/builds/0/bom/ngm/lfric/lfric_atm-fab FC=ifort ./scripts/grab_lfric.py
 
 # build lfric_atm
 #imagerun FAB_WORKSPACE=$PWD FC=ifort ./scripts/atm.py
-imagerun FAB_WORKSPACE=/scratch/hc46/hc46_gitlab/builds/$CI_RUNNER_SHORT_TOKEN/0/bom/ngm/lfric/lfric_atm-fab/ FC=ifort ./scripts/atm.py
+imagerun FAB_WORKSPACE=/scratch/hc46/hc46_gitlab/builds/0/bom/ngm/lfric/lfric_atm-fab/ FC=ifort ./scripts/atm.py
 
-mv lfric_source_${rev}/source/lfric/lfric_atm/example /scratch/hc46/hc46_gitlab/builds/$CI_RUNNER_SHORT_TOKEN/0/bom/ngm/lfric/lfric_atm-fab
-mv lfric_source_${rev}/source/lfric/lfric_atm/metadata /scratch/hc46/hc46_gitlab/builds/$CI_RUNNER_SHORT_TOKEN/0/bom/ngm/lfric/lfric_atm-fab
+mv lfric_source_${rev}/source/lfric/lfric_atm/example /scratch/hc46/hc46_gitlab/builds/0/bom/ngm/lfric/lfric_atm-fab
+mv lfric_source_${rev}/source/lfric/lfric_atm/metadata /scratch/hc46/hc46_gitlab/builds/0/bom/ngm/lfric/lfric_atm-fab
 
-cp atm_ifort_1stage/lfric_atm.exe /scratch/hc46/hc46_gitlab/builds/$CI_RUNNER_SHORT_TOKEN/0/bom/ngm/lfric/lfric_atm-fab
+cp atm_ifort_1stage/lfric_atm.exe /scratch/hc46/hc46_gitlab/builds/0/bom/ngm/lfric/lfric_atm-fab
