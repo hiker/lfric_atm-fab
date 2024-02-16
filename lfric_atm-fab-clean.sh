e0180ca8fbe66f1b63a28da10a320bbaafa5bca3
davelee2804 <davelee2804@gmail.com>
2024-02-16 12:58:06 +1100

    cleaning out the lfric cylc-run dirs

--- lfric_atm-fab-clean.sh
+++ lfric_atm-fab-clean.sh
@@ -7,3 +7,7 @@ rm -rf 1/bom/ngm/lfric/lfric_atm-fab
 rm -rf 2/bom/ngm/lfric/lfric_atm-fab
 rm -rf 3/bom/ngm/lfric/lfric_atm-fab
 rm -rf /scratch/hc46/hc46_gitlab/builds/0/bom/ngm/lfric/lfric_atm-fab
+rm -rf /scratch/hc46/hc46_gitlab/cylc-run/gungho_nightly
+rm -rf /scratch/hc46/hc46_gitlab/cylc-run/lfric_atm_developer
+rm -rf /scratch/hc46/hc46_gitlab/cylc-run/lfric_atm_nightly
+rm -rf /scratch/hc46/hc46_gitlab/cylc-run/lfric_apps-nightly
