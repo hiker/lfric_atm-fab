6d21613e57d177acc94652e1248d17218f711f9d
davelee2804 <davelee2804@gmail.com>
2024-02-20 10:52:39 +1100

    modifying the job submission process to leverage of the exising ci-qsub script and then check the output based on the resulting job.log file

--- lfric_atm-fab-check.py
+++ lfric_atm-fab-check.py
@@ -1,14 +1,19 @@
 import sys
 import os
 
-fp = open('PET0.lfric_atm.Log','r')
+fp = open('job.log','r')
 
 failed = 1
 for line in fp:
     line = line.split()
+    if len(line) < 3:
+        continue
     if line[-2] == 'lfric_atm' and line[-1] == 'completed.':
         print('lfric_atm run succeeded')
         failed = 0
 fp.close()
+
+if failed:
+    print('lfric_atm run failed')
 
 sys.exit(failed)
--- lfric_atm-fab-run.sh
+++ lfric_atm-fab-run.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-qsub lfric_atm-fab-run.pbs
-found=0
-while [ $found == 0 ]
-do
-  test -f timer.txt && found=1
-done
-echo "Run done!"
+BIN_DIR=/scratch/hc46/hc46_gitlab/builds/$CI_RUNNER_SHORT_TOKEN/0/bom/ngm/lfric/lfric_atm-fab/atm_ifort_1stage
+echo $BIN_DIR
+
+rev=$(rev < "lfric_revision")
+echo $rev
+
+CONFIG_DIR=/scratch/hc46/hc46_gitlab/builds/$CI_RUNNER_SHORT_TOKEN/0/bom/ngm/lfric/lfric_atm-fab/lfric_source_${rev}/source/lfric/lfric_atm/example/
+echo $CONFIG_DIR
+
+module use /scratch/hc46/hc46_gitlab/ngm/modules/
+module load lfric-v0/intel-openmpi-lfric-fab
+
+cd example
+imagerun mpirun -np 1 ../lfric_atm.exe configuration.nml
