d2e04a0da7ff19fdd98e99520c798752e085503e
davelee2804 <davelee2804@gmail.com>
2024-02-26 10:32:03 +1100

    removed deletion of other lfric ci pipelines

--- lfric_atm-fab-clean.sh
+++ lfric_atm-fab-clean.sh
@@ -6,11 +6,3 @@ rm -rf 0/bom/ngm/lfric/lfric_atm-fab
 rm -rf 1/bom/ngm/lfric/lfric_atm-fab
 rm -rf 2/bom/ngm/lfric/lfric_atm-fab
 rm -rf 3/bom/ngm/lfric/lfric_atm-fab
-rm -rf 0/bom/ngm/lfric/lfric-atm-developer
-rm -rf 0/bom/ngm/lfric/lfric_apps-nightly
-rm -rf 0/bom/ngm/lfric/gungho_nightly
-rm -rf /scratch/hc46/hc46_gitlab/builds/0/bom/ngm/lfric/lfric_atm-fab
-rm -rf /scratch/hc46/hc46_gitlab/cylc-run/gungho_nightly
-rm -rf /scratch/hc46/hc46_gitlab/cylc-run/lfric_atm_developer
-rm -rf /scratch/hc46/hc46_gitlab/cylc-run/lfric_atm_nightly
-rm -rf /scratch/hc46/hc46_gitlab/cylc-run/lfric_apps-nightly
