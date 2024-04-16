ca43fa2ca4df2c4ce1d7c1b60460fa577c89fc58
Junwei Lyu <jl8309@gadi-login-04.gadi.nci.org.au>
2024-04-17 09:28:12 +1000

    Fixed path to grab.py script and added debug print outs

--- lfric_atm-fab-build.sh
+++ lfric_atm-fab-build.sh
@@ -19,24 +19,41 @@ module load lfric-v0/intel-openmpi-fab-new-framework
 # grab the lfric sources
 export FAB_WORKSPACE=$PWD
 
-imagerun FAB_WORKSPACE=$PWD FC=ifort ./fab_framework/infrastructure/grab_lfric.py
+imagerun FAB_WORKSPACE=$PWD FC=ifort ./fab_framework/infrastructure/build/fab/grab_lfric.py
+
+echo "Grabbed the lfric sources successfully"
 
 # install the fab build scripts
 ./fab_framework/install.sh $FAB_WORKSPACE/lfric_source_${lfric_core_rev}/source/lfric_core $FAB_WORKSPACE/lfric_source_${lfric_core_rev}/source/lfric_apps
 
+echo "Installed the fab build scripts"
+
+echo "Start building apps"
+
 # build skeleton
 imagerun FAB_WORKSPACE=$PWD FC=ifort PYTHONPATH=$PYTHONPATH:$FAB_WORKSPACE/lfric_source_${lfric_core_rev}/source/lfric_core/infrastructure/build/psyclone $FAB_WORKSPACE/lfric_source_${lfric_core_rev}/source/lfric_core/miniapps/skeleton/fab_mini_skeleton.py
+
+echo "Built skeleton"
 
 # build gungho_model
 imagerun FAB_WORKSPACE=$PWD FC=ifort PYTHONPATH=$PYTHONPATH:$FAB_WORKSPACE/lfric_source_${lfric_core_rev}/source/lfric_core/infrastructure/build/psyclone $FAB_WORKSPACE/lfric_source_${lfric_core_rev}/source/lfric_apps/applications/gungho_model/fab_gungho_model.py
 
+echo "Built gungho_model"
+
 # build gravity_wave
 imagerun FAB_WORKSPACE=$PWD FC=ifort PYTHONPATH=$PYTHONPATH:$FAB_WORKSPACE/lfric_source_${lfric_core_rev}/source/lfric_core/infrastructure/build/psyclone $FAB_WORKSPACE/lfric_source_${lfric_core_rev}/source/lfric_apps/applications/gravity_wave/fab_gravity_wave.py
 
+echo "Built gravity_wave"
+
 # build lfric_atm
 imagerun FAB_WORKSPACE=$PWD FC=ifort PYTHONPATH=$PYTHONPATH:$FAB_WORKSPACE/lfric_source_${lfric_core_rev}/source/lfric_core/infrastructure/build/psyclone $FAB_WORKSPACE/lfric_source_${lfric_core_rev}/source/lfric_apps/applications/lfric_atm/fab_lfric_atm.py
+
+echo "Built lfric_atm"
 
 # build lfric_inputs
 imagerun FAB_WORKSPACE=$PWD FC=ifort PYTHONPATH=$PYTHONPATH:$FAB_WORKSPACE/lfric_source_${lfric_core_rev}/source/lfric_core/infrastructure/build/psyclone $FAB_WORKSPACE/lfric_source_${lfric_core_rev}/source/lfric_apps/applications/lfricinputs/fab_lfric2um.py
 imagerun FAB_WORKSPACE=$PWD FC=ifort PYTHONPATH=$PYTHONPATH:$FAB_WORKSPACE/lfric_source_${lfric_core_rev}/source/lfric_core/infrastructure/build/psyclone $FAB_WORKSPACE/lfric_source_${lfric_core_rev}/source/lfric_apps/applications/lfricinputs/um2lfric.py
 
+echo "Built lfric_inputs"
+
+echo "Finished building"
--- lfric_atm-fab-run.sh
+++ lfric_atm-fab-run.sh
@@ -16,6 +16,8 @@ export lfric_apps_rev=$(rev < "lfric_apps_revision")
 echo "lfric_apps_revison = ${lfric_apps_rev}"
 
 # run gungho_model
+echo "Start running gungho_model"
+
 export BIN_DIR=/scratch/hc46/hc46_gitlab/builds/$CI_RUNNER_SHORT_TOKEN/0/bom/ngm/lfric/lfric_atm-fab/gungho_model-ifort
 echo "bin_dir = ${BIN_DIR}"
 
@@ -24,7 +26,11 @@ echo "config_dir = ${CONFIG_DIR}"
 
 imagerun mpirun -np 4 $BIN_DIR/gungho_model $CONFIG_DIR/configuration.nml
 
+echo "Finished running gungho_model"
+
 # run lfric_atm
+echo "Start running lfric_atm"
+
 export BIN_DIR=/scratch/hc46/hc46_gitlab/builds/$CI_RUNNER_SHORT_TOKEN/0/bom/ngm/lfric/lfric_atm-fab/lfric_atm-ifort
 echo "bin_dir = ${BIN_DIR}"
 
@@ -32,3 +38,7 @@ export CONFIG_DIR=/scratch/hc46/hc46_gitlab/builds/$CI_RUNNER_SHORT_TOKEN/0/bom/
 echo "config_dir = ${CONFIG_DIR}"
 
 imagerun mpirun -np 1 $BIN_DIR/lfric_atm $CONFIG_DIR/configuration.nml
+
+echo "Finished running lfric_atm"
+
+echo "Finished all runs"
