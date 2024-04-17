ce786560b78892646aa4be606cdb5f7cd7dca4dd
Junwei Lyu <jl8309@gadi-login-04.gadi.nci.org.au>
2024-04-17 11:16:39 +1000

    Added infrastructure/build/fab to PYTHONPATH as sys.insert does not seem to work with the container

--- lfric_atm-fab-build.sh
+++ lfric_atm-fab-build.sh
@@ -57,31 +57,31 @@ ls
 
 echo "Start building apps"
 
-export PYTHONPATH=/opt/spack/.local/lib/python3.12/site-packages/
+export PYTHONPATH=/opt/spack/.local/lib/python3.12/site-packages/:$FAB_WORKSPACE/lfric_source_${lfric_core_rev}/source/lfric_core/infrastructure/build/psyclone:$FAB_WORKSPACE/lfric_source_${lfric_core_rev}/source/lfric_core/infrastructure/build/fab
 
 # build skeleton
-imagerun FAB_WORKSPACE=$PWD FC=ifort PYTHONPATH=$PYTHONPATH:$FAB_WORKSPACE/lfric_source_${lfric_core_rev}/source/lfric_core/infrastructure/build/psyclone $FAB_WORKSPACE/lfric_source_${lfric_core_rev}/source/lfric_core/miniapps/skeleton/fab_mini_skeleton.py
+imagerun FAB_WORKSPACE=$PWD FC=ifort PYTHONPATH=$PYTHONPATH $FAB_WORKSPACE/lfric_source_${lfric_core_rev}/source/lfric_core/miniapps/skeleton/fab_mini_skeleton.py
 
 echo "Built skeleton"
 
 # build gungho_model
-imagerun FAB_WORKSPACE=$PWD FC=ifort PYTHONPATH=$PYTHONPATH:$FAB_WORKSPACE/lfric_source_${lfric_core_rev}/source/lfric_core/infrastructure/build/psyclone $FAB_WORKSPACE/lfric_source_${lfric_core_rev}/source/lfric_apps/applications/gungho_model/fab_gungho_model.py
+imagerun FAB_WORKSPACE=$PWD FC=ifort PYTHONPATH=$PYTHONPATH $FAB_WORKSPACE/lfric_source_${lfric_core_rev}/source/lfric_apps/applications/gungho_model/fab_gungho_model.py
 
 echo "Built gungho_model"
 
 # build gravity_wave
-imagerun FAB_WORKSPACE=$PWD FC=ifort PYTHONPATH=$PYTHONPATH:$FAB_WORKSPACE/lfric_source_${lfric_core_rev}/source/lfric_core/infrastructure/build/psyclone $FAB_WORKSPACE/lfric_source_${lfric_core_rev}/source/lfric_apps/applications/gravity_wave/fab_gravity_wave.py
+imagerun FAB_WORKSPACE=$PWD FC=ifort PYTHONPATH=$PYTHONPATH $FAB_WORKSPACE/lfric_source_${lfric_core_rev}/source/lfric_apps/applications/gravity_wave/fab_gravity_wave.py
 
 echo "Built gravity_wave"
 
 # build lfric_atm
-imagerun FAB_WORKSPACE=$PWD FC=ifort PYTHONPATH=$PYTHONPATH:$FAB_WORKSPACE/lfric_source_${lfric_core_rev}/source/lfric_core/infrastructure/build/psyclone $FAB_WORKSPACE/lfric_source_${lfric_core_rev}/source/lfric_apps/applications/lfric_atm/fab_lfric_atm.py
+imagerun FAB_WORKSPACE=$PWD FC=ifort PYTHONPATH=$PYTHONPATH $FAB_WORKSPACE/lfric_source_${lfric_core_rev}/source/lfric_apps/applications/lfric_atm/fab_lfric_atm.py
 
 echo "Built lfric_atm"
 
 # build lfric_inputs
-imagerun FAB_WORKSPACE=$PWD FC=ifort PYTHONPATH=$PYTHONPATH:$FAB_WORKSPACE/lfric_source_${lfric_core_rev}/source/lfric_core/infrastructure/build/psyclone $FAB_WORKSPACE/lfric_source_${lfric_core_rev}/source/lfric_apps/applications/lfricinputs/fab_lfric2um.py
-imagerun FAB_WORKSPACE=$PWD FC=ifort PYTHONPATH=$PYTHONPATH:$FAB_WORKSPACE/lfric_source_${lfric_core_rev}/source/lfric_core/infrastructure/build/psyclone $FAB_WORKSPACE/lfric_source_${lfric_core_rev}/source/lfric_apps/applications/lfricinputs/um2lfric.py
+imagerun FAB_WORKSPACE=$PWD FC=ifort PYTHONPATH=$PYTHONPATH $FAB_WORKSPACE/lfric_source_${lfric_core_rev}/source/lfric_apps/applications/lfricinputs/fab_lfric2um.py
+imagerun FAB_WORKSPACE=$PWD FC=ifort PYTHONPATH=$PYTHONPATH $FAB_WORKSPACE/lfric_source_${lfric_core_rev}/source/lfric_apps/applications/lfricinputs/um2lfric.py
 
 echo "Built lfric_inputs"
 
