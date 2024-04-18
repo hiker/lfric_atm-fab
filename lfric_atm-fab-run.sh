f8f3872c8aac7823b43fa76f440006df7fd88b8e
Junwei Lyu <jl8309@gadi-login-02.gadi.nci.org.au>
2024-04-18 13:43:49 +1000

    Changed run ncpus to be 6 for gungho_model(it can only be run with 1 or multiples of 6)

--- lfric_atm-fab-run.sh
+++ lfric_atm-fab-run.sh
@@ -24,7 +24,7 @@ echo "Start running gungho_model"
 cd $FAB_WORKSPACE/lfric_source_${lfric_core_rev}/source/lfric_apps/applications/gungho_model/example
 echo 'current dir'
 echo $PWD
-imagerun mpirun -np 4 $FAB_WORKSPACE/gungho_model-ifort/gungho_model configuration.nml
+imagerun mpirun -np 6 $FAB_WORKSPACE/gungho_model-ifort/gungho_model configuration.nml
 echo "Finished running gungho_model"
 
 # run lfric_atm
