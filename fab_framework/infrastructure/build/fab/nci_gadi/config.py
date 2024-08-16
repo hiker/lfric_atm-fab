b73030f8d6e58aa274cdf97b4c6541c4250068d3
Joerg Henrichs <henrichsjoerg@gmail.com>
2024-08-16 15:53:35 +1000

    Converted the tau wrapper to use the new CompilerWrapper class.

--- fab_framework/infrastructure/build/fab/nci_gadi/config.py
+++ fab_framework/infrastructure/build/fab/nci_gadi/config.py
@@ -1,36 +1,44 @@
 #! /usr/bin/env python3
 
-from fab.tools import Gcc, Gfortran, Icc, Ifort, ToolRepository
+'''This module contains the default configuration for NCI. It will be invoked
+by the Baf scripts. This script:
+- sets intel-classic as the default compiler suite to use.
+- Adds the tau compiler wrapper as (optional) compilers to the ToolRepository.
+
+'''
+
+
+from fab.tools import (Category, Compiler, CompilerWrapper, ToolRepository)
 
 from default.config import Config as DefaultConfig
 
 
-class TauGnuFortran(Gfortran):
-    def __init__(self):
-        super().__init__(name="tau-gnu-fortran",
-                         exec_name="tau_f90.sh")
+class Tauf90(CompilerWrapper):
+    '''Class for the Tau profiling Fortran compiler wrapper.
+    It will be using the name "tau-COMPILER_NAME", but will call tau_f90.sh.
+
+    :param compiler: the compiler that the tau_f90.sh wrapper will use.
+    '''
+
+    def __init__(self, compiler: Compiler):
+        super().__init__(name=f"tau-{compiler.name}",
+                         exec_name="tau_f90.sh", compiler=compiler, mpi=True)
 
 
-class TauIntelFortran(Ifort):
-    def __init__(self):
-        super().__init__(name="tau-intel-fortran",
-                         exec_name="tau_f90.sh")
+class Taucc(CompilerWrapper):
+    '''Class for the Tau profiling C compiler wrapper.
+    It will be using the name "tau-COMPILER_NAME", but will call tau_cc.sh.
 
+    :param compiler: the compiler that the tau_cc.sh wrapper will use.
+    '''
 
-class TauGnuC(Gcc):
-    def __init__(self):
-        super().__init__(name="tau-gnu-c",
-                         exec_name="tau_cc.sh")
-
-
-class TauIntelC(Icc):
-    def __init__(self):
-        super().__init__(name="tau-intel-c",
-                         exec_name="tau_cc.sh")
+    def __init__(self, compiler: Compiler):
+        super().__init__(name=f"tau-{compiler.name}",
+                         exec_name="tau_cc.sh", compiler=compiler, mpi=True)
 
 
 class Config(DefaultConfig):
-    '''For NCI, add the Tau wrapper.
+    '''For NCI, make intel the default, and add the Tau wrapper.
     '''
 
     def __init__(self):
@@ -38,7 +46,12 @@ class Config(DefaultConfig):
         tr = ToolRepository()
         tr.set_default_compiler_suite("intel-classic")
 
-        # A compiler will also be added automatically
-        # as linker, i.e. linker-tau-gnu etc
-        for tool in [TauGnuFortran, TauIntelFortran, TauGnuC, TauIntelC]:
-            tr.add_tool(tool())
+        # Add the tau wrappers for Fortran and C. Note that add_tool
+        # will automatically add them as a linker as well.
+        for ftn in ["ifort", "gfortran"]:
+            compiler = tr.get_tool(Category.FORTRAN_COMPILER, ftn)
+            tr.add_tool(Tauf90(compiler))
+
+        for cc in ["icc", "gcc"]:
+            compiler = tr.get_tool(Category.C_COMPILER, cc)
+            tr.add_tool(Taucc(compiler))
