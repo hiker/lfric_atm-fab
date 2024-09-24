a399d21b038951c7d34f98558a9ac494a28dff10
Junwei Lyu <junwei.lyu@bom.gov.au>
2024-09-24 17:41:19 +1000

    Changed check to check only lfric_atm

--- lfric_atm-fab-check.py
+++ lfric_atm-fab-check.py
@@ -8,25 +8,19 @@ print(f"current dir: {os.getcwd()}")
 fp = open('job.log','r')
 
 failed = 1
-gungho_failed = 1
 lfric_atm_failed = 1
 for line in fp:
     line = line.split()
     if len(line) < 3:
         continue
-    if line [-3] == 'Finished' and line[-2] == 'running' and line[-1] == 'gungho_model':
-        print('gungho_model run succeeded')
-        gungho_failed = 0    
     if line[-2] == 'lfric_atm' and line[-1] == 'completed.':
         print('lfric_atm run succeeded')
         lfric_atm_failed = 0
-if gungho_failed == 0 and lfric_atm_failed == 0:
+if lfric_atm_failed == 0:
     failed = 0 
 fp.close()
 
 if failed:
-    if gungho_failed:
-        print('gungho_model run failed')
     if lfric_atm_failed:
         print('lfric_atm run failed')
         
